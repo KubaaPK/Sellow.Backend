8b3ca638e2fff75db1c4817a9415e1c8de0f7c01
Jakub Szkowrytko <k.szkowrytko@gmail.com>
2024-10-04 14:53:18 +0200

    feat: add Email and Username value objects

---
+++ src/Shared/Sellow.Shared.Abstractions/SharedKernel/ValueObjects/Email.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using System.Text.RegularExpressions;
+using Sellow.Shared.Abstractions.Exceptions;
+
+namespace Sellow.Shared.Abstractions.SharedKernel.ValueObjects;
+
+public sealed record Email
+{
+    private static readonly Regex EmailRegex = new(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+
+    public string Value { get; }
+
+    public Email(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new InvalidEmailException(value);
+        }
+
+        value = value.ToLowerInvariant().Trim();
+
+        if (EmailRegex.IsMatch(value) is false)
+        {
+            throw new InvalidEmailException(value);
+        }
+
+        Value = value;
+    }
+
+    public static implicit operator Email(string email) => new(email);
+
+    public static implicit operator string(Email email) => email.Value;
+}
+
+public sealed class InvalidEmailException : SellowException
+{
+    public override HttpStatusCode HttpCode => HttpStatusCode.BadRequest;
+    public override string ErrorCode => "invalid_email";
+
+    public InvalidEmailException(string email) : base($"Email '{email}' is invalid.")
+    {
+    }
+}
---
+++ src/Shared/Sellow.Shared.Abstractions/SharedKernel/ValueObjects/Username.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using Sellow.Shared.Abstractions.Exceptions;
+
+namespace Sellow.Shared.Abstractions.SharedKernel.ValueObjects;
+
+public sealed record Username
+{
+    private const int MinimumUsernameLength = 3;
+    private const int MaximumUsernameLength = 25;
+
+    public string Value { get; }
+
+    public Username(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new InvalidUsernameException(value);
+        }
+
+        value = value.Trim();
+
+        if (value.Length is < MinimumUsernameLength or > MaximumUsernameLength)
+        {
+            throw new InvalidUsernameException(value);
+        }
+
+        Value = value;
+    }
+
+    public static implicit operator Username(string username) => new(username);
+
+    public static implicit operator string(Username username) => username.Value;
+}
+
+public sealed class InvalidUsernameException : SellowException
+{
+    public override HttpStatusCode HttpCode => HttpStatusCode.BadRequest;
+    public override string ErrorCode => "invalid_username";
+
+    public InvalidUsernameException(string username) : base($"Username '{username}' is invalid.")
+    {
+    }
+}
