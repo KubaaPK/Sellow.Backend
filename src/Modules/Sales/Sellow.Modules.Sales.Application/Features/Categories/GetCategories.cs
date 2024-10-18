1dc17004a79a5926d2c53a2d9d1cfa58e9b03d2e
Jakub Szkowrytko <k.szkowrytko@gmail.com>
2024-10-18 08:39:07 +0200

    feat: cache categories list

--- src/Modules/Sales/Sellow.Modules.Sales.Application/Features/Categories/GetCategories.cs
+++ src/Modules/Sales/Sellow.Modules.Sales.Application/Features/Categories/GetCategories.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Sellow.Modules.Sales.Infrastructure.DAL;
 
 namespace Sellow.Modules.Sales.Application.Features.Categories;
@@ -9,15 +10,27 @@ internal sealed class GetCategories : IRequest<IEnumerable<CategoryDto>>;
 internal sealed class GetCategoriesHandler : IRequestHandler<GetCategories, IEnumerable<CategoryDto>>
 {
     private readonly SalesDbContext _context;
+    private readonly IMemoryCache _memoryCache;
 
-    public GetCategoriesHandler(SalesDbContext context)
+    public GetCategoriesHandler(SalesDbContext context, IMemoryCache memoryCache)
     {
         _context = context;
+        _memoryCache = memoryCache;
     }
 
     public async Task<IEnumerable<CategoryDto>> Handle(GetCategories request, CancellationToken cancellationToken)
     {
+        if (_memoryCache.Get("Sales/Categories") is not null)
+        {
+            return _memoryCache.Get<IEnumerable<CategoryDto>>("Sales/Categories") ?? Array.Empty<CategoryDto>();
+        }
+
         var categories = await _context.Categories.ToListAsync(cancellationToken);
-        return CategoryDto.BuildCategoryTree(categories);
+
+        var categoriesDto = CategoryDto.BuildCategoryTree(categories);
+
+        _memoryCache.Set("Sales/Categories", categoriesDto);
+
+        return categoriesDto;
     }
 }
